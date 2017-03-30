b5823f0379834e799b91ad85b594904dc3b331ea
App Academy Student <student@appacademy.io>
2017-03-30 15:31:16 -0700

    Add MinesweeperGame class and add flag for Board

---
+++ game.rb
@@ -0,0 +1,8 @@
+require_relative 'board.rb'
+
+class MinesweeperGame
+  def initialize(board = Board.new)
+    @board = board
+  end
+
+end
--- tile.rb
+++ tile.rb
@@ -2,14 +2,21 @@ class Tile
   attr_reader :bomb, :revealed
   def initialize(is_bomb = false)
     @bomb = is_bomb
-    @revealed = is_bomb
+    @revealed = false
+    @flagged = false
   end
 
   def rig_bomb
     @bomb = true
   end
 
+  def toggle_flag
+    @flagged = !@flagged
+  end
+
   def to_s
-    @bomb ? "*" : "-"
+    return "F" if @flagged
+    return "B" if @bomb # TODO remove debug
+    return "*" unless @revealed
   end
 end
